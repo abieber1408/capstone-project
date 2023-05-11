c89fec3e47cebb78e6d59fb7c956f6a4e8633739
Anita Bieber <abieber1111gmail.com>
2023-05-11 18:23:27 +0200

    correct return, imports

--- pages/api/subjects/[id].js
+++ pages/api/subjects/[id].js
@@ -1,15 +1,15 @@
 import dbConnect from "../../../db/connect";
-import Subject from "../../../db/models/Subject.js";
+import Subject from "../../../db/models/Subject";
 
 export default async function handler(request, response) {
     await dbConnect();
     const { id } = request.query;
     
     if (request.method === "GET"){
-        const subject= await Subject.findById(id);
+        const subject = Subject.findById(id);
         if (!subject) {
             return response.status(404).json({ status: "Not found 🤷🏼 " });
         }
-        response.status(200).json(subject);
+        return response.status(200).json(subject);
     }
 }
--- pages/api/subjects/index.js
+++ pages/api/subjects/index.js
@@ -1,14 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 //import { subjects } from "../../../lib/subjects";
 import dbConnect from "../../../db/connect";
-import Subject from "../../../db/models/Subject.js";
+import Subject from "../../../db/models/Subject";
 
 export default async function handler(request, response) {
     await dbConnect();
 
     if (request.method === "GET") {
         const subjects = await Subject.find()
-        response.status(200).json(subjects);
+        return response.status(200).json(subjects);
     }
 }
   
